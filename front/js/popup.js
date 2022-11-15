af44251160c921c20a211df5dc152a0649955c63
Julien <julien.pinard@epitech.eu>
2022-11-15 14:58:35 +0100

    update button donation

--- front/js/popup.js
+++ front/js/popup.js
@@ -1,9 +1,25 @@
 let btn = document.getElementById("sendDonation");
+let radios_btn = document.getElementsByClassName("form-check-input");
+const form = document.getElementById("donation_form");
+let formData = new FormData(form);
+
+
+for (let i = 0; i < radios_btn.length; i++) {
+    let radio_btn = radios_btn[i];
+    radio_btn.addEventListener("click", (e) => {
+        checkAmount(e);
+    })
+}
+
+function checkAmount(e) {
+    if(e.target.value !== undefined){
+        console.log(e.target.value)
+        btn.removeAttribute("disabled")
+    }
+}
 
 btn.onclick = function sendDonation() {
 
-    const form = document.getElementById("donation_form");
-    let formData = new FormData(form);
     const formDataObj = {};
 
     formData.forEach((value, key) => (formDataObj[key] = value));
