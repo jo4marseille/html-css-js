940acfab7bffcc7424ea5fb9063c734d3c78cd46
Julien <julien.pinard@epitech.eu>
2022-11-15 11:38:21 +0100

    add popup donation

--- front/js/popup.js
+++ front/js/popup.js
@@ -1,3 +1,20 @@
-$('#donation').on('shown.bs.modal', function () {
-    $('#btn_donation').trigger('focus')
-})
+let btn = document.getElementById("sendDonation");
+
+btn.onclick = function sendDonation() {
+
+    const form = document.getElementById("donation_form");
+    let formData = new FormData(form);
+    const formDataObj = {};
+
+    formData.forEach((value, key) => (formDataObj[key] = value));
+
+    formDataObj["sport"] = "football";
+
+    console.log(formDataObj)
+
+    axios.post("http://localhost:3000/api/donation", formDataObj)
+        .then(data => {
+            console.log(data);
+        })
+
+}
